501c52c6e242c2c36a58363364bd3a186da0acd5
willf80 <willyfkouadio@gmail.com>
2019-12-09 09:38:18 +0100

    Return the current time in UTC format when date parameter is empty

--- services/timestamp-service.js
+++ services/timestamp-service.js
@@ -1,17 +1,22 @@
 const parseDate = (dateString, callback) => {
+  if (!dateString) {
+    callback(null, new Date())
+    return
+  }
+
   let date
   if (/^\d+$/.test(dateString)) {
     date = new Date(+dateString)
   } else {
     date = new Date(dateString)
   }
-  
+
   if (!date) {
     const error = 'Invalid Date'
     callback(error)
     return
   }
-  
+
   callback(null, date)
 }
 
